3c1a3f864e59be467efcd1c221c1d8b14347146e
Jim Shaw <jimxshaw@gmail.com>
2017-05-13 16:58:15 -0500

    Cleaned up index.html.

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MyTitle from 'MyTitle';
+import MyTitle from './MyTitle';
+
+var div = React.DOM.div;
 
 var MyTitleFactory = React.createFactory(MyTitle);
 
