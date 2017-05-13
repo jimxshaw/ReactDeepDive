a2d5f9bbaf6cd38519dfd414b30605bbca35ed57
Jim Shaw <jimxshaw@gmail.com>
2017-05-13 16:22:51 -0500

    Used standard es linting.

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,31 +1,33 @@
-var div = React.DOM.div;
-var h1 = React.DOM.h1;
+/* global React ReactDom */
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
     // Every react component must contain a render method and
     // that method must return markup.
-    render: function () {
-        return (
+  render: function () {
+    return (
             div(null,
                 h1({style: {color: this.props.color, fontWeight: 'bold'}}, this.props.title)
             )
-        );
-    }
-});
+    )
+  }
+})
 
-var MyTitleFactory = React.createFactory(MyTitle);
+var MyTitleFactory = React.createFactory(MyTitle)
 
 var MyFirstComponent = React.createClass({
-    render: function () {
-        return (
+  render: function () {
+    return (
             div(null,
                 MyTitleFactory({title: 'props are awesome', color: 'lavender'}),
                 MyTitleFactory({title: 'hello world', color: 'maroon'}),
                 MyTitleFactory({title: 'learning react', color: 'rebeccapurple'}),
                 MyTitleFactory({title: 'this is a test', color: 'limegreen'})
             )
-        );
-    }
-});
+    )
+  }
+})
 
-ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'));
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
