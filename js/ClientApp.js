29990af689ce9ee1f6e4752ece3a00f0eb5e5ad0
Jim Shaw <jimxshaw@gmail.com>
2017-05-21 15:01:52 -0500

    Added more es lint rules to webpack module config.

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { render } from 'react-dom'
+import {render} from 'react-dom'
 import '../public/normalize.css'
 import '../public/style.css'
 
 const App = React.createClass({
   render () {
     return (
-            <div className='app'>
-                <div className='landing'>
-                    <h1>jsvideo</h1>
-                    <input type='text' placeholder='Search' />
-                    <a>or Browse All</a>
-                </div>
-            </div>
-        )
+      <div className='app'>
+        <div className='landing'>
+          <h1>jsvideo</h1>
+          <input type='text' placeholder='Search' />
+          <a>or Browse All</a>
+        </div>
+      </div>
+    )
   }
 })
 
