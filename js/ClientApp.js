913c1d4da7b16c95de18ed3f7d1edb20e525e132
Jim Shaw <jimxshaw@gmail.com>
2017-05-21 14:47:56 -0500

    Added .eslintrc.json and configured linting npm script in project.json.

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { render } from 'react-dom';
-import '../public/normalize.css';
-import '../public/style.css';
-
+import React from 'react'
+import { render } from 'react-dom'
+import '../public/normalize.css'
+import '../public/style.css'
 
 const App = React.createClass({
-    render() {
-        return (
+  render () {
+    return (
             <div className='app'>
                 <div className='landing'>
                     <h1>jsvideo</h1>
@@ -14,8 +13,8 @@ const App = React.createClass({
                     <a>or Browse All</a>
                 </div>
             </div>
-        );
-    }
-});
+        )
+  }
+})
 
-render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById('app'))
